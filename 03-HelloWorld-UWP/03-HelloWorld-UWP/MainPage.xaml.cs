003018a14235f566c1950bfbbac66eecb1d18e9d
jolsensei <horusensei@gmail.com>
2018-10-04 14:43:56 +0200

    Acabar el nuevo ejercicio

---
+++ 03-HelloWorld-UWP/03-HelloWorld-UWP/MainPage.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Popups;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// La plantilla de elemento Página en blanco está documentada en https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0xc0a
+
+namespace _03_HelloWorld_UWP
+{
+    /// <summary>
+    /// Página vacía que se puede usar de forma independiente o a la que se puede navegar dentro de un objeto Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void TextBlock_SelectionChanged(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        /// <summary>
+        /// Lanza un MessageDialong con un saludo, usando el contenido introducido
+        /// en el TextBox
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            String nombre;
+            MessageDialog popup;
+
+            nombre = txtNombre.Text;
+
+            popup = new MessageDialog($"Hola {nombre}");
+
+            await popup.ShowAsync();
+
+        }
+    }
+}
