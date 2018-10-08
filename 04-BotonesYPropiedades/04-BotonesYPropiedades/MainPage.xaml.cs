a8d722221cba233c2dfacaba1eccb99873e2bdea
jolsensei <horusensei@gmail.com>
2018-10-08 12:43:55 +0200

    Acabado

--- 04-BotonesYPropiedades/04-BotonesYPropiedades/MainPage.xaml.cs
+++ 04-BotonesYPropiedades/04-BotonesYPropiedades/MainPage.xaml.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -33,12 +32,47 @@ namespace _04_BotonesYPropiedades
 
 
             //Asignacion de propiedades al boton
-            boton.Content = "Boton3";
-            boton.VerticalAlignment = VerticalAlignment.Center;
-            boton.HorizontalAlignment = HorizontalAlignment.Center;
+<<<<<<< Updated upstream
             boton.Height = 70;
             boton.Width = 200;
-            
+            boton.Font = 
+=======
+<<<<<<< HEAD
+            boton.Content = "Boton 3";
+            boton.Height = 70;
+            boton.Width = 200;
+            boton.FontSize = 16;
+            boton.VerticalAlignment = VerticalAlignment.Center;
+            boton.HorizontalAlignment = HorizontalAlignment.Center;
+            boton.Background = new SolidColorBrush(Windows.UI.Colors.Blue);
+            boton.BorderBrush = new SolidColorBrush(Windows.UI.Colors.Yellow);
+            boton.FontFamily = new FontFamily("verdana");
+            boton.FontWeight = Windows.UI.Text.FontWeights.Bold;
+
+            sp_panel.Children.Add(boton);
+
+            //Añadimos un metodo al boton
+            boton.Click += boton_Click;
+
+
+        }
+
+        /// <summary>
+        /// Evento asociado al boton que mostrara un mensaje
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        public async void boton_Click(object sender, RoutedEventArgs e) {
+
+            MessageDialog mensage = new MessageDialog("Hola");
+
+            await mensage.ShowAsync();
+=======
+            boton.Height = 70;
+            boton.Width = 200;
+            boton.Font = 
+>>>>>>> parent of 9ba2865... Peta, solucionar mañana
+>>>>>>> Stashed changes
 
         }
 
