4260e98d53bfaef9f11ab3d1a8597da2d5d4f8f3
jolsensei <horusensei@gmail.com>
2018-10-29 14:36:23 +0100

    Pues esto se termina para el miercoles

---
+++ 14-SplitViewPractice/14-SplitViewPractice/Views/MainPage.xaml.cs
@@ -0,0 +1,61 @@
+﻿using _14_SplitViewPractice.Views;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// La plantilla de elemento Página en blanco está documentada en https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0xc0a
+
+namespace _14_SplitViewPractice
+{
+    /// <summary>
+    /// Página vacía que se puede usar de forma independiente o a la que se puede navegar dentro de un objeto Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void HamburgerButton_Click(object sender, RoutedEventArgs e)
+        {
+            MySplitView.IsPaneOpen = !MySplitView.IsPaneOpen;
+        }
+
+        private void HomeListBoxItem_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+            this.frame.Navigate(typeof(home));
+            Titulo.Text = "Home";
+
+            if (MySplitView.IsPaneOpen) {
+
+                MySplitView.IsPaneOpen = !MySplitView.IsPaneOpen;
+            }
+
+           
+        }
+
+        private void MensaggesListBoxItem_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+            this.frame.Navigate(typeof(messages));
+            Titulo.Text = "Messages";
+
+            if (MySplitView.IsPaneOpen)
+            {
+
+                MySplitView.IsPaneOpen = !MySplitView.IsPaneOpen;
+            }
+        }
+    }
+}
